0ec77e75cbbc4e86308bd84184069d819ff68f87
tirumalarajajee <tirumala.rajajee@gmail.com>
2026-01-21 10:48:34 +0530

    Add files via upload

---
+++ state.js
@@ -0,0 +1,14 @@
+// state.js
+console.log("state.js LOADED");
+
+export const caseGroups = {
+  ulcer: [],
+  patch: [],
+  growth: [],
+  mucosa: [],
+  pigmentation: [],
+  symptom: [],
+  habit: [],
+  teeth: [],
+  illness: []
+};
